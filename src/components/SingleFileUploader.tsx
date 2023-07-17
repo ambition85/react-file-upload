65676f67e38c3b72bba9e9ffd0f9696be80ab3f6
ambition85 <ambition9385@gmail.com>
2023-07-18 00:52:39 +0900

    SingleFileUploader

---
+++ src/components/SingleFileUploader.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+
+const SingleFileUploader = () => {
+  const [file, setFile] = useState<File | null>(null)
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0])
+    }
+  }
+
+  const handleUpload = async () => {
+    // We will fill this out later
+  }
+
+  return (
+    <>
+      <div>
+        <label htmlFor="file" className="sr-only">
+          Choose a file
+        </label>
+        <input id="file" type="file" onChange={handleFileChange} />
+      </div>
+      {file && (
+        <section>
+          File details:
+          <ul>
+            <li>Name: {file.name}</li>
+            <li>Type: {file.type}</li>
+            <li>Size: {file.size} bytes</li>
+          </ul>
+        </section>
+      )}
+
+      {file && <button onClick={handleUpload}>Upload a file</button>}
+    </>
+  )
+}
+
+export default SingleFileUploader
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import SingleFileUploader from './components/SingleFileUploader.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <SingleFileUploader />
   </React.StrictMode>,
 )
